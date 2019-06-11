e578fe6edc1d511c85dc99bb85cdaa43574dec9c
Thiago Yudi Fukunaga <thiagoyudifukunaga@gmail.com>
2019-06-11 09:48:19 -0400

    Add reactotron

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import './config/ReactotronConfig';
+
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View, Button } from "react-native";
 
 import Todo from "./components/Todo";
+import { toUnicode } from 'punycode';
+
+console.tron.log("Hello World")
 
 export default class App extends Component {
   state = {
